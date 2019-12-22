b084b639ca134783a9ad4609271e96e07c605ca2
Konda Madhusudhan <konda.madhusudhan@olacabs.com>
2019-12-22 18:38:26 +0530

    first commit

---
+++ src/com/madhu/psds/visitingallpoints/MinimumTime.java
@@ -0,0 +1,12 @@
+package com.madhu.psds.visitingallpoints;
+
+public class MinimumTime {
+    public static void nain(String args[]) {
+
+    }
+
+    public int minTimeToVisitAllPoints(int[][] points) {
+
+        return 0;
+    }
+}
