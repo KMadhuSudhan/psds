10700c1c2fbfc241ac471cb8671287113ec5effc
Konda Madhusudhan <konda.madhusudhan@olacabs.com>
2019-12-22 19:22:30 +0530

    minTimeToVisitAllPoints

--- src/com/madhu/psds/visitingallpoints/MinimumTime.java
+++ src/com/madhu/psds/visitingallpoints/MinimumTime.java
@@ -1,12 +1,15 @@
 package com.madhu.psds.visitingallpoints;
 
 public class MinimumTime {
-    public static void nain(String args[]) {
-
+    public static void main(String args[]) {
+        int[][] points = new int[][]{{1, 1}, {3, 4}, {-1, 0}};
+        System.out.print(minTimeToVisitAllPoints(points));
     }
 
-    public int minTimeToVisitAllPoints(int[][] points) {
-
-        return 0;
+    private static int minTimeToVisitAllPoints(int[][] points) {
+        int time = 0;
+        for(int i = 0; i < points.length - 1; i++)
+            time += Math.max( Math.abs(points[i][0] - points[i+1][0]), Math.abs(points[i][1] - points[i+1][1]));
+        return time;
     }
 }
